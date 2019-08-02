52a408ad23792a7d743b9dbf809367baaa7fef9e
chenyiwenglish <chenyiwenglish@163.com>
2019-08-02 15:42:03 +0800

    leetcode 1/1134

---
+++ src/main/java/com/chenyiwenglish/leetcodejava/LeetcodeJavaApplication.java
@@ -0,0 +1,13 @@
+package com.chenyiwenglish.leetcodejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetcodeJavaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/chenyiwenglish/leetcodejava/easy/TwoSum.java
@@ -0,0 +1,27 @@
+package com.chenyiwenglish.leetcodejava.easy;
+
+import java.util.Arrays;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = solution.twoSum(nums, target);
+        Arrays.stream(result).forEach(System.out::println);
+    }
+
+    static class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            for (int i = 0; i < nums.length; i ++) {
+                for (int j = i + 1; j < nums.length; j ++) {
+                    if (nums[i] + nums[j] == target) {
+                        return new int[] {i, j};
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
